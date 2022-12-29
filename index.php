df7388e3161bdb1e0a3832dacf869ccfa700f466
Guilherme <guilhermesouzabenso@gmail.com>
2022-12-29 13:28:55 -0300

    Novas alterações

--- index.php
+++ index.php
@@ -11,8 +11,11 @@
 
 <body>
     <div class="container">
+        <div class="title">
+                <span>Veneno Store</span>
+            </div>
         <div class="box">
-            <span>Veneno Store</span>
+            
             <a id="login" href="Login/login.php"> Login </a>
             <a id="creat" href="Creat/Creat.php">Creat Account</a>
         </div>
