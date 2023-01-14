679078fc497d5a278792e98dc63be7bd073c5283
PsycoCrazy <gordo.welington20@gmail.com>
2023-01-14 20:16:08 -0300

    Alterações

--- Funcionamento/Jogos/Jogos.php
+++ Funcionamento/Jogos/Jogos.php
@@ -13,8 +13,18 @@
         <input class="btn-home" type='button' value='Home' onclick='history.go(-1)' href="Creat.css" />
         </div>
         <div>
-        <a class="bnt-planeta" href="Planeta/Planeta.php">Save The Planet</a>
+        <a class="bnt-planeta" id="Detalhe" href="Planeta/Planeta.php">Save The Planet</a>
         </div>
     </div>
+    <script type="text/javascript" src="Jogos.js"></script>
+        <script type="text/javascript">
+	    VanillaTilt.init(document.querySelectorAll("#Detalhe"), {
+		max: 25,
+		speed: 400
+	    });
+	
+	//It also supports NodeList
+	VanillaTilt.init(document.querySelectorAll(".your-element"));
+</script>
 </body>
 </html>
