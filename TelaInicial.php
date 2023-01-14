e2cd43e3d9ce928d58850128eff9e2664794b857
PsycoCrazy <gordo.welington20@gmail.com>
2023-01-14 17:50:17 -0300

    Alterações

--- TelaInicial.php
+++ TelaInicial.php
@@ -28,39 +28,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mundo Virtual</title>
+    <link rel="stylesheet" href="TelaInicial.css">
 </head>
 <body>
 
     <?php
         echo "<h1>BEM VINDO <u>$logado</u>, ESPERO QUE SE DIVIRTA</h1>";
     ?>
-    <div class="box-search">
-        <input type="search" class="form-control w-25" placeholder="Pesquisar" id="Pesquisar">
-        <button onclick="searchData()" class="btn btn-primary">search</button>
+    <div>
+        <a href="sair.php" class="btn-sair">Sair</a>
     </div>
     <div>
-        <a href="sair.php" class="btn btn-sair">Sair</a>
+        <a href="MeuPerfil/Perfil.php" class="btn-Perfil">Meu Perfil</a>
+        <a class="bnt-Aplicativos" id="Detalhe" href="Funcionamento/aplicativos/Aplicativos.php">Aplicativos</a>
+        <a class="btn-Jogos" id="Detalhe" href="Funcionamento/Jogos/Jogos.php">Jogos</a>
     </div>
-    <div>
-        <a href="MeuPerfil/Perfil.php" class="btn btn-Perfil">Meu Perfil</a>
-    </div>
-    <script>
-        var search = document.getElementById('Pesquisar');
-
-        search.addEventListener("keydown", function(event){
-            if (event.key === "Enter")
-            {
-                searchData();
-            }
-        });
-        function searchData()
-        {
-            window.location = 'TelaInicial.php?search=' +search.value;
-        }
-    </script>
-    <div>
-        <a href="Funcionamento/aplicativos/Aplicativos.php">Aplicativos</a>
-        <a href="Funcionamento/Jogos/Jogos.php">Jogos</a>
-    </div>
+    <script type="text/javascript" src="TelaInicial.js"></script>
+        <script type="text/javascript">
+	    VanillaTilt.init(document.querySelectorAll("#Detalhe"), {
+		max: 25,
+		speed: 400
+	    });
+	
+	//It also supports NodeList
+	VanillaTilt.init(document.querySelectorAll(".your-element"));
+</script>
 </body>
 </html>
