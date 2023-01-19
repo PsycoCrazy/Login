03876b7ac03c90e0e60d8da35796fe14991ff481
PsycoCrazy <gordo.welington20@gmail.com>
2023-01-19 20:59:02 -0300

    Jogo da Forca

---
+++ Funcionamento/Jogos/JogoDaForca/Forca.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Forca.css">
+  <title>Jogo da Forca</title>
+</head>
+<body>
+  <div>
+    <input class="btn-home" type='button' value='Home' onclick='history.go(-1)' href="Forca.css" />
+  </div>
+  <div class="container">
+    <h1>Jogo da Forca</h1>
+      <hr />
+    
+  </div>
+</body>
+</html>
--- Funcionamento/Jogos/Jogos.php
+++ Funcionamento/Jogos/Jogos.php
@@ -12,12 +12,13 @@
 </head>
 <body>
     <div>
-        <input class="btn-home" type='button' value='Home' onclick='history.go(-1)' href="Creat.css" />
+        <input class="btn-home" type='button' value='Home' onclick='history.go(-1)' href="Jogos.css" />
     </div>
     <div class="containers">
         <div>
         <a class="bnt-planeta" id="Detalhe" href="Planeta/Planeta.php">Save The Planet</a>
         <a class="velha" id="Detalhe" href="JogoDaVelha/Velha.php">Jogo da Velha</a>
+        <a class="forca" id="Detalhe" href="JogoDaForca/Forca.php">Jogo Da Forca</a>
         </div>
     </div>
     <script type="text/javascript" src="Jogos.js"></script>
