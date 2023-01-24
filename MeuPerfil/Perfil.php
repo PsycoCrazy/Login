586f923a11a80bba4aa08e4bf0fd639931a91b73
PsycoCrazy <gordo.welington20@gmail.com>
2023-01-23 22:09:01 -0300

    Alterações

--- MeuPerfil/Perfil.php
+++ MeuPerfil/Perfil.php
@@ -16,48 +16,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="Perfil.css">
 </head>
 <body>
+    <div class="containers">
     <div>
-    <input type="hidden" name="ID" value="<?php echo $ID ?>">
+        <input class="btn-home" type='button' value='Home' onclick='history.go(-1)' href="Perfil.css" />
     </div>
-    <div class="m-5">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Data_Nasc</th>
-                    <th scope="col">Sexo</th>
-                    <th scope="col">Senha</th>
-                    <th scope="col">...</th>
-                </tr>
-            </thead>
-                <tbody>
-                    <?php
-                        while($user_data = mysqli_fetch_assoc($result))
-                        {
-                            echo "<tr>";
-                            echo "<td>".$user_data['ID']."</td>";
-                            echo "<td>".$user_data['Nome']."</td>";
-                            echo "<td>".$user_data['Email']."</td>";
-                            echo "<td>".$user_data['Data_Nasc']."</td>";
-                            echo "<td>".$user_data['Sexo']."</td>";
-                            echo "<td>".$user_data['Senha']."</td>";
-                            echo "<td>
-                                <a class='btn btn-editar' href='Editar.php?ID=$user_data[ID]';>Editar</a>
-                                </td>";
-                            echo "<td>
-                                <a class='btn btn-deletar' href='Editar.php?ID$user_data[ID]';>Deletar</a>
+        <div>
+            <input type="hidden" name="ID" value="<?php echo $ID ?>">
+        </div>
+        <div class="m-5">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Data_Nasc</th>
+                        <th scope="col">Sexo</th>
+                        <th scope="col">Senha</th>
+                        <th scope="col">...</th>
+                    </tr>
+                </thead>
+                    <tbody>
+                        <?php
+                            while($user_data = mysqli_fetch_assoc($result))
+                            {
+                                echo "<tr>";
+                                echo "<td>".$user_data['ID']."</td>";
+                                echo "<td>".$user_data['Nome']."</td>";
+                                echo "<td>".$user_data['Email']."</td>";
+                                echo "<td>".$user_data['Data_Nasc']."</td>";
+                                echo "<td>".$user_data['Sexo']."</td>";
+                                echo "<td>".$user_data['Senha']."</td>";
+                                echo "<td>
+                                    <a class='btn btn-editar' href='Editar.php?ID=$user_data[ID]';>Editar</a>
+                                    </td>";
+                                echo "<td>
+                                    <a class='btn btn-deletar' href='Editar.php?ID$user_data[ID]';>Deletar</a>
                             </td>";
-                            echo "</tr>";   
-                        }
-                    ?>
-                </tbody>
+                        echo "</tr>";   
+                    }
+                ?>
+            </tbody>
         </table>
     </div>
-
+</div>
 
 </body>
 </html>
